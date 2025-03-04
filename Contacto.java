767bc8c2a0532a568b4b3d50ad21532c06a85ffd
Anton1oC4astr0 <143725409+Anton1oC4astr0@users.noreply.github.com>
2025-03-03 22:28:05 -0600

    Add files via upload

---
+++ Contacto.java
@@ -0,0 +1,40 @@
+
+package unidad2;
+
+public class Contacto {
+    //Creamos los atributos del contacto
+    private String nombre;
+    private int telefono;
+
+    public Contacto(String nombre, int telefono) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+    public Contacto(String nombre) {
+        this.nombre = nombre;
+        this.telefono = 0;// Valor de inicio por si no tengo ningun contacto
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int getTelefono() {
+        return telefono;
+    }
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    } 
+    public boolean equals(Contacto contacto){//No pueden existir contactos con el mismo nombre
+        if (nombre.trim().equalsIgnoreCase(contacto.getNombre().trim())){//trim()= borrar espacios en blanco de este atributo
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public String toString() {//El método toString te devuelve una representación en cadena de texto del objeto sobre el cual lo invoques. Es muy útil cuando quieres devolver un resumen del estado interno del objeto. 
+        return "Nombre: "+ nombre + "Telefono: " + telefono;
+    }
+    
+}
